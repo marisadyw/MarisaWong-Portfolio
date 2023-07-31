8984aacd8ab651ad25544ed8f001dc3eac16268a
marisadyw <marisadyw@gmail.com>
2023-07-30 22:28:33 -0400

    testing rebuilding the rmarkdown files at deployment

--- R/build.R
+++ R/build.R
@@ -1,2 +1,5 @@
 # An optional custom script to run before Hugo builds your site.
 # You can delete it if you do not need it.
+
+library(blogdown)
+blogdown::build_site(build_rmd = TRUE, run_hugo = FALSE)
